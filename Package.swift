fae6afbc8807bf670aa246557d07eb70b172f65e
Ryan Foster <ryan.foster@publix.com>
2023-05-24 13:25:36 -0400

    KMM SPM package release for 0.1.5

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 // BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
-let remoteKotlinUrl = "https://pkgs.dev.azure.com/ryanjfoster94/_packaging/ryanjfoster94/maven/v1/empty-kmm-lib-test/shared-kmmbridge/0.1.3/shared-kmmbridge-0.1.3.zip"
+let remoteKotlinUrl = "https://pkgs.dev.azure.com/ryanjfoster94/_packaging/ryanjfoster94/maven/v1/empty-kmm-lib-test/shared-kmmbridge/0.1.5/shared-kmmbridge-0.1.5.zip"
 let remoteKotlinChecksum = "52bd72c9ede724abdf2c1368e6c90bf84d1a95839a367781e8f3d8e07ad6f8ba"
 let packageName = "PublixAnalyticsLogger"
 // END KMMBRIDGE BLOCK
